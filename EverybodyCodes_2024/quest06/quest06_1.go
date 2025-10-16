c44f879981c1d77a7ea9130911ba5f998098dfcc
blfuentes <blfuentes@mundo-r.com>
2025-10-16 18:39:34 +0200

    working: 20240601

--- EverybodyCodes_2024/quest06/quest06_1.go
+++ EverybodyCodes_2024/quest06/quest06_1.go
@@ -2,19 +2,26 @@ package quest06
 
 import (
 	"everybodycodes_2024/utilities"
-	"fmt"
 	"strings"
 )
 
+type Node struct {
+	Name   string
+	Leaves []Node
+}
+
 func Executepart1() int {
-	// var fileName string = "./quest06/test_input_01.txt"
-	var fileName string = "./quest06/quest06_1.txt"
+	var fileName string = "./quest06/test_input_01.txt"
+	// var fileName string = "./quest06/quest06_1.txt"
 	var result int = 0
 
 	// parsing
 	if fileContent, err := utilities.ReadFileAsLines(fileName); err == nil {
 		numRows, numCols := len(fileContent), len(strings.Split(fileContent[0], " "))
-		fmt.Printf("%v, %v", numRows, numCols)
+		nodes := make([]Node, 0)
+		for _, line := range fileContent {
+
+		}
 	}
 
 	return result
