58429b14f1a9796de3b5c559b69d46d7f7061f17
blfuentes <blfuentes@mundo-r.com>
2025-10-17 20:18:56 +0200

    finished: 20240701, 20240702

--- EverybodyCodes_2024/quest07/quest07_1.go
+++ EverybodyCodes_2024/quest07/quest07_1.go
@@ -2,15 +2,36 @@ package quest07
 
 import (
 	"everybodycodes_2024/utilities"
+	"strings"
 )
 
-func Executepart1() int {
-	var fileName string = "./quest07/test_input_01.txt"
-	// var fileName string = "./quest07/quest07_1.txt"
-	var result int = 0
-	if _, err := utilities.ReadFileAsText(fileName); err == nil {
+func Executepart1() string {
+	// var fileName string = "./quest07/test_input_01.txt"
+	var fileName string = "./quest07/quest07_1.txt"
+	var result string = ""
 
+	chariots := make(map[string]int)
+	rules := make(map[string][]string)
+	if fileContent, err := utilities.ReadFileAsLines(fileName); err == nil {
+		for _, line := range fileContent {
+			charName, parts := strings.Split(line, ":")[0], strings.Split(strings.Split(line, ":")[1], ",")
+			rules[charName] = parts
+			startingValue := 10
+			sum := 0
+			for idx := 0; idx < 10; idx++ {
+				sign := rules[charName][idx%len(parts)]
+				switch sign {
+				case "+":
+					startingValue++
+				case "-":
+					startingValue--
+				}
+				sum += startingValue
+			}
+			chariots[charName] = sum
+		}
+
+		result = SortByValue(chariots)
 	}
 	return result
 }
-
--- EverybodyCodes_2024/quest07/utils07.go
+++ EverybodyCodes_2024/quest07/utils07.go
@@ -1,2 +1,30 @@
 package quest07
 
+import (
+	"sort"
+	"strings"
+)
+
+func SortByValue(chariots map[string]int) string {
+	type kv struct {
+		k string
+		v int
+	}
+	arr := make([]kv, 0, len(chariots))
+	for k, v := range chariots {
+		arr = append(arr, kv{k, v})
+	}
+
+	sort.Slice(arr, func(i, j int) bool {
+		if arr[i].v == arr[j].v {
+			return arr[i].k < arr[j].k // tie-breaker: lexical order
+		}
+		return arr[i].v > arr[j].v // highest value first
+	})
+
+	var sb strings.Builder
+	for _, e := range arr {
+		sb.WriteString(e.k)
+	}
+	return sb.String()
+}
