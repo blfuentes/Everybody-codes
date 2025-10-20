388886018e990ed12ea1e7b047f12576b957d5c9
blfuentes <epiphonetodie@gmail.com>
2025-10-20 13:24:19 +0200

    finished: 20241001 | 20241002

--- EverybodyCodes_2024_FSharp/quest10/quest10_2.fs
+++ EverybodyCodes_2024_FSharp/quest10/quest10_2.fs
@@ -1,10 +1,100 @@
 module quest10_part02
 
 open EverybodyCodes_2024_FSharp.Modules
+open System.Collections.Generic
 
-let path = "quest10/test_input_02.txt"
-// let path = "quest10/quest10_input_02.txt"
+//let path = "quest10/test_input_02.txt"
+let path = "quest10/quest10_input_02.txt"
+
+type Defintion = {
+    ShrineMap: char[,]
+    ShrineSolved: char[,]
+    RunesByRow: Dictionary<int, Set<char>>
+    RunesByCol: Dictionary<int, Set<char>>
+    ToFill: (int * int) list
+}
+
+let parseFull (lines: string[]) =
+    let blockHeight = (lines.Length + 1) / 9
+    let blockWidth = (lines[0].Length + 1) / 9
+
+    let allRunes =
+        seq {
+            for row in 0..blockHeight - 1 do
+                for col in 0..blockWidth - 1 do
+                    let shrineMap = Array2D.create 8 8 '.'
+                    let shrineSolved = Array2D.create 8 8 '.'
+                    let runesByRow = new Dictionary<int, Set<char>>()
+                    let runesByCol = new Dictionary<int, Set<char>>()
+                    let toFill =
+                        seq {
+                            for rIdx in 0..7 do
+                                for cIdx in 0..7 do
+                                    let char = lines[rIdx + row * 9][cIdx + col * 9]
+                                    shrineMap[rIdx, cIdx] <- char
+                                    shrineSolved[rIdx, cIdx] <- char  
+                                    if char = '.' then
+                                        yield (rIdx, cIdx)
+                        } |> Seq.toList
+                    for row in 0 .. shrineMap.GetLength(0) - 1 do
+                        let rowRunes = 
+                            shrineMap[row, *] 
+                            |> Seq.filter (fun c -> c <> '.') 
+                            |> Set.ofSeq
+                        runesByRow.Add(row, rowRunes)
+                    for col in 0 .. shrineMap.GetLength(1) - 1 do
+                        let colRunes = 
+                            shrineMap[*, col] 
+                            |> Seq.filter (fun c -> c <> '.') 
+                            |> Set.ofSeq
+                        runesByCol.Add(col, colRunes)
+                    yield { 
+                        ShrineMap = shrineMap;
+                        ShrineSolved = shrineSolved;
+                        RunesByRow = runesByRow;
+                        RunesByCol = runesByCol;
+                        ToFill = toFill 
+                    }
+        } |> Seq.toList
+    allRunes
+
+let printMap(themap: char[,]) =
+    let rows = themap.GetLength(0)
+    let cols = themap.GetLength(1)
+    for r in 0 .. rows - 1 do
+        for c in 0 .. cols - 1 do
+            printf "%c" themap[r, c]
+        printfn ""
+
+let findRune (pos: int*int) (themap: char[,]) (runesByRow: Dictionary<int, Set<char>>) (runesByCol: Dictionary<int, Set<char>>)=
+    let rowRunes = runesByRow[fst pos]
+    let colRunes = runesByCol[snd pos]
+    let rune = Set.intersect rowRunes colRunes |> Set.minElement
+    themap[fst pos, snd pos] <- rune
+    rune
+
+let calculatePower (runes: char array) =
+    let weights = [|'A'..'Z'|] |> Array.mapi (fun i c -> (c, i + 1)) |> dict
+    runes 
+    |> Array.mapi(fun i c -> 
+        weights[c] * (i + 1)) 
+    |> Array.sum
+
 
 let execute() =
     let lines = LocalHelper.GetLinesFromFile(path)
-    0
+    let allRunes = parseFull lines
+    allRunes
+    |> List.map(fun runeDef ->
+        let toFill = runeDef.ToFill
+        let shrineSolved = runeDef.ShrineSolved
+        let runesByRow = runeDef.RunesByRow
+        let runesByCol = runeDef.RunesByCol
+        let findRuneLocal = findRune >> (fun f -> f shrineSolved runesByRow runesByCol)
+        let runes = 
+            toFill
+            |> Seq.map (fun pos -> findRuneLocal pos)
+            |> Seq.toArray
+        calculatePower runes
+    )
+    |> Seq.sum
