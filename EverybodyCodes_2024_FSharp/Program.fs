497e09f41f00bd4d747d3ee9ea474d355eee695f
blfuentes <epiphonetodie@gmail.com>
2025-10-24 09:19:16 +0200

    prepared: 202413

--- EverybodyCodes_2024_FSharp/Program.fs
+++ EverybodyCodes_2024_FSharp/Program.fs
@@ -1,4 +1,4 @@
-﻿open System
+open System
 
 open EverybodyCodes_2024_FSharp.Modules
 
@@ -9,45 +9,45 @@ let ms ticks =
 [<EntryPoint>]
 let main argv =
 
-    //// DAY 00
-    //let (resultquest00Part1, time00_1) = duration quest00_part01.execute
-    //printfn "Final result Quest 00 part 1: %A in %s" resultquest00Part1 (ms time00_1)
-    //let (resultquest00Part2, time00_2) = duration quest00_part02.execute
-    //printfn "Final result Quest 00 part 2: %A in %s" resultquest00Part2 (ms time00_2)
-    //let (resultquest00Part3, time00_3) = duration quest00_part03.execute
-    //printfn "Final result Quest 00 part 3: %A in %s" resultquest00Part3 (ms time00_3)
+    // DAY 00
+    let (resultquest00Part1, time00_1) = duration quest00_part01.execute
+    printfn "Final result Quest 00 part 1: %A in %s" resultquest00Part1 (ms time00_1)
+    let (resultquest00Part2, time00_2) = duration quest00_part02.execute
+    printfn "Final result Quest 00 part 2: %A in %s" resultquest00Part2 (ms time00_2)
+    let (resultquest00Part3, time00_3) = duration quest00_part03.execute
+    printfn "Final result Quest 00 part 3: %A in %s" resultquest00Part3 (ms time00_3)
 
-    //// DAY 08
-    //let (resultquest08Part1, time08_1) = duration quest08_part01.execute
-    //printfn "Final result Quest 08 part 1: %A in %s" resultquest08Part1 (ms time08_1)
-    //let (resultquest08Part2, time08_2) = duration quest08_part02.execute
-    //printfn "Final result Quest 08 part 2: %A in %s" resultquest08Part2 (ms time08_2)
-    //let (resultquest08Part3, time08_3) = duration quest08_part03.execute
-    //printfn "Final result Quest 08 part 3: %A in %s" resultquest08Part3 (ms time08_3)
+    // DAY 08
+    let (resultquest08Part1, time08_1) = duration quest08_part01.execute
+    printfn "Final result Quest 08 part 1: %A in %s" resultquest08Part1 (ms time08_1)
+    let (resultquest08Part2, time08_2) = duration quest08_part02.execute
+    printfn "Final result Quest 08 part 2: %A in %s" resultquest08Part2 (ms time08_2)
+    let (resultquest08Part3, time08_3) = duration quest08_part03.execute
+    printfn "Final result Quest 08 part 3: %A in %s" resultquest08Part3 (ms time08_3)
 
-    //// DAY 09
-    //let (resultquest09Part1, time09_1) = duration quest09_part01.execute
-    //printfn "Final result Quest 09 part 1: %A in %s" resultquest09Part1 (ms time09_1)
-    //let (resultquest09Part2, time09_2) = duration quest09_part02.execute
-    //printfn "Final result Quest 09 part 2: %A in %s" resultquest09Part2 (ms time09_2)
-    //let (resultquest09Part3, time09_3) = duration quest09_part03.execute
-    //printfn "Final result Quest 09 part 3: %A in %s" resultquest09Part3 (ms time09_3)
+    // DAY 09
+    let (resultquest09Part1, time09_1) = duration quest09_part01.execute
+    printfn "Final result Quest 09 part 1: %A in %s" resultquest09Part1 (ms time09_1)
+    let (resultquest09Part2, time09_2) = duration quest09_part02.execute
+    printfn "Final result Quest 09 part 2: %A in %s" resultquest09Part2 (ms time09_2)
+    let (resultquest09Part3, time09_3) = duration quest09_part03.execute
+    printfn "Final result Quest 09 part 3: %A in %s" resultquest09Part3 (ms time09_3)
 
-    //// DAY 10
-    //let (resultquest10Part1, time10_1) = duration quest10_part01.execute
-    //printfn "Final result Quest 10 part 1: %A in %s" resultquest10Part1 (ms time10_1)
-    //let (resultquest10Part2, time10_2) = duration quest10_part02.execute
-    //printfn "Final result Quest 10 part 2: %A in %s" resultquest10Part2 (ms time10_2)
-    //let (resultquest10Part3, time10_3) = duration quest10_part03.execute
-    //printfn "Final result Quest 10 part 3: %A in %s" resultquest10Part3 (ms time10_3)
+    // DAY 10
+    let (resultquest10Part1, time10_1) = duration quest10_part01.execute
+    printfn "Final result Quest 10 part 1: %A in %s" resultquest10Part1 (ms time10_1)
+    let (resultquest10Part2, time10_2) = duration quest10_part02.execute
+    printfn "Final result Quest 10 part 2: %A in %s" resultquest10Part2 (ms time10_2)
+    let (resultquest10Part3, time10_3) = duration quest10_part03.execute
+    printfn "Final result Quest 10 part 3: %A in %s" resultquest10Part3 (ms time10_3)
 
-    //// DAY 11
-    //let (resultquest11Part1, time11_1) = duration quest11_part01.execute
-    //printfn "Final result Quest 11 part 1: %A in %s" resultquest11Part1 (ms time11_1)
-    //let (resultquest11Part2, time11_2) = duration quest11_part02.execute
-    //printfn "Final result Quest 11 part 2: %A in %s" resultquest11Part2 (ms time11_2)
-    //let (resultquest11Part3, time11_3) = duration quest11_part03.execute
-    //printfn "Final result Quest 11 part 3: %A in %s" resultquest11Part3 (ms time11_3)
+    // DAY 11
+    let (resultquest11Part1, time11_1) = duration quest11_part01.execute
+    printfn "Final result Quest 11 part 1: %A in %s" resultquest11Part1 (ms time11_1)
+    let (resultquest11Part2, time11_2) = duration quest11_part02.execute
+    printfn "Final result Quest 11 part 2: %A in %s" resultquest11Part2 (ms time11_2)
+    let (resultquest11Part3, time11_3) = duration quest11_part03.execute
+    printfn "Final result Quest 11 part 3: %A in %s" resultquest11Part3 (ms time11_3)
 
     // DAY 12
     let (resultquest12Part1, time12_1) = duration quest12_part01.execute
@@ -56,5 +56,13 @@ let main argv =
     printfn "Final result Quest 12 part 2: %A in %s" resultquest12Part2 (ms time12_2)
     let (resultquest12Part3, time12_3) = duration quest12_part03.execute
     printfn "Final result Quest 12 part 3: %A in %s" resultquest12Part3 (ms time12_3)
+
+    // DAY 13
+    let (resultquest13Part1, time13_1) = duration quest13_part01.execute
+    printfn "Final result Quest 13 part 1: %A in %s" resultquest13Part1 (ms time13_1)
+    let (resultquest13Part2, time13_2) = duration quest13_part02.execute
+    printfn "Final result Quest 13 part 2: %A in %s" resultquest13Part2 (ms time13_2)
+    let (resultquest13Part3, time13_3) = duration quest13_part03.execute
+    printfn "Final result Quest 13 part 3: %A in %s" resultquest13Part3 (ms time13_3)
 
     0 // return an integer exit code
---
+++ EverybodyCodes_2024_FSharp/quest13/quest13_3.fs
@@ -0,0 +1,10 @@
+module quest13_part03
+
+open EverybodyCodes_2024_FSharp.Modules
+
+let path = "quest13/test_input_03.txt"
+//let path = "quest13/quest13_input_03.txt"
+
+let execute() =
+    let lines = LocalHelper.GetLinesFromFile(path)
+    0
