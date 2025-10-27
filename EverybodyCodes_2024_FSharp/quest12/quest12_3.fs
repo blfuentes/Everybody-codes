681117fa57b183b6adcc863f0355b245b55a8d60
blfuentes <epiphonetodie@gmail.com>
2025-10-27 15:32:15 +0100

    finished: 20241203

--- EverybodyCodes_2024_FSharp/quest12/quest12_3.fs
+++ EverybodyCodes_2024_FSharp/quest12/quest12_3.fs
@@ -1,241 +1,161 @@
 ﻿module quest12_part03
 
-open System.Collections.Generic
 open EverybodyCodes_2024_FSharp.Modules
 
 //let path = "quest12/test_input_03.txt"
 let path = "quest12/quest12_input_03.txt"
 
-type Position = {
-    Row: int
-    Col: int
-}
+type Entity = { Pos: float * float; Val: char }
 
-type Target = {
-    Life: int
-    Pos: Position
-}
+let parseContent (catapultLayout: string) (targetLines: string array) =
+    let catapults =
+        catapultLayout.Split('\n')
+        |> Array.mapi (fun y line ->
+            line.ToCharArray()
+            |> Array.mapi (fun x ch ->
+                if ['A'; 'B'; 'C'] |> List.contains ch then
+                    Some { Pos = (float x, float y); Val = ch }
+                else None
+            )
+        )
+        |> Array.concat
+        |> Array.choose id
+        |> Array.toList
 
-type Segment = {
-    Name: string
-    Ranking: int
-    Pos: Position
-}
+    let aPos = (catapults |> List.find (fun c -> c.Val = 'A')).Pos
 
-type Tower = {
-    Segments: Segment list
-}
-
-let towers = new Dictionary<int, Tower>()
-
-let parseContent(lines: string array) =
-    lines
-    |> Array.map (fun line ->
-        { Life = 1; Pos = { Row = int(line.Split(" ")[1]); Col = int(line.Split(" ")[0]) } }
-    ) |> Array.toList
+    let targets =
+        targetLines
+        |> Array.map (fun line ->
+            let parts = line.Split(' ')
+            let x, y = float parts[0], float parts[1]
+            { Pos = (fst aPos + x, snd aPos - y); Val = 'T' }
+        )
+        |> Array.toList
     
-let positionsMemory = new Dictionary<int * int, Segment list>()
+    (catapults, targets)
 
+let vectUR = (1.0, -1.0)
+let vectR  = (1.0, 0.0)
+let vectDR = (1.0, 1.0)
+let vectDL = (-1.0, 1.0)
 
-//let possibleHits (tower: Tower) (power: int) (floorline: int) =
-//    let hitForSegment (segment: Segment) =
-//        let goingUp = 
-//            [1..power]
-//            |> List.map (fun p -> { Row = segment.Pos.Row - p; Col = segment.Pos.Col + p})
-//        let middle = 
-//            [1..power]
-//            |> List.map (fun p -> { Row = segment.Pos.Row - power; Col = segment.Pos.Col + p + power})
-//        let mutable height = segment.Pos.Row - power
-//        let mutable column = 1
-//        let goingDown =
-//            seq {
-//                while height < floorline do
-//                    yield { Row = height + 1; Col = segment.Pos.Col + (2 * power) + column}
-//                    height <- height + 1
-//                    column <- column + 1
-//            } |> Seq.toList
-//        (segment, goingUp @ middle @ goingDown)
-//    tower.Segments
-//    |> List.map hitForSegment
+let private areFloatsEqual a b = abs (a - b) < 1e-9
 
-let possibleHits (tower: Tower) (power: int) (time: int) (floorline: int) =
-    if positionsMemory.ContainsKey(power, time) then
-        positionsMemory[(power, time)]
-    else
-        let segmentPositions = 
-            seq {
-                for segment in tower.Segments do
-                    let t = time
-                    let p = power
+let canHitTarget (fromPos: float * float) (_toPos: float * float) (moving: int) (shift: int) (maxPower: int) =
+    let mutable result = None
+    let mutable power = 1
 
-                    // Phase 1: Up-Right movement
-                    let upRightSteps = min t p
-                    let deltaRowUp = upRightSteps
-                    let deltaColUp = upRightSteps
+    let toPos =
+        if shift = 1 then (fst _toPos + fst vectDL, snd _toPos + snd vectDL)
+        else _toPos
 
-                    // Phase 2: Horizontal movement
-                    let rightSteps = max 0 (min (t - p) p)
-                    let deltaRowRight = 0
-                    let deltaColRight = rightSteps
+    while result.IsNone && power < maxPower do
+        let p = float power
+        let m = float moving
+        let (fromX, fromY) = fromPos
+        let (toX, toY) = toPos
+        let (urX, urY) = vectUR
+        let (dlX, dlY) = vectDL
 
-                    // Phase 3: Down-Right movement
-                    let downRightSteps = max 0 (t - 2 * p)
-                    let deltaRowDown = -downRightSteps
-                    let deltaColDown = downRightSteps
+        // Phase 1: Up-Right
+        let denX1 = urX - m * dlX
+        let denY1 = urY - m * dlY
+        if denX1 <> 0.0 && denY1 <> 0.0 then
+            let tX = (toX - fromX) / denX1
+            let tY = (toY - fromY) / denY1
+            if areFloatsEqual tX tY && areFloatsEqual tX (round tX) && tX > 0.0 && tX <= p then
+                result <- Some power
 
-                    let finalRow = segment.Pos.Row + deltaRowUp + deltaRowRight + deltaRowDown
-                    let finalCol = segment.Pos.Col + deltaColUp + deltaColRight + deltaColDown
-                    
-                    if finalRow >= floorline then
-                        yield { segment with Pos = { Row = finalRow; Col = finalCol } }
-                    else
-                        // If below floorline, yield a position that won't match any target
-                        yield { segment with Pos = { Row = -1; Col = -1 } }
-                        
-            } |> Seq.toList
-        positionsMemory.Add((power, time), segmentPositions)
-        segmentPositions
+        if result.IsNone then
+            let from2 = (fromX + p * urX, fromY + p * urY)
+            let to2 = (toX + m * p * dlX, toY + m * p * dlY)
+            let (rX, rY) = vectR
+            let denX2 = rX - m * dlX
+            let denY2 = rY - m * dlY
 
-let destroyTargets (tower: Tower) (targets: Target list) =
-    let floorline = -1
-    let rec destroy (toDestroy: Target list) (value: int) =
-        match toDestroy with
-        | [] ->
-            value
-        | head :: tail ->
-            printfn "Destroying target at Row %i Col %i" head.Pos.Row head.Pos.Col
-            let mutable counterTime = 0
-            let mutable doContinue = true
-            let mutable toBeHit = head
-            let mutable currentValue = 0
-            let mutable bestRanking = 4
-            let mutable power = 1
-            let maxPower = 250
-            while doContinue do
-                power <- 1
-                if counterTime % 500 = 0 then
-                    printfn "\nPower %i Time %i Target at Row %i Col %i" power counterTime toBeHit.Pos.Row toBeHit.Pos.Col
-                while power <= maxPower do
-                    //if power % 1000 = 0 then 
-                    let pHits = possibleHits tower power counterTime floorline
-                    //printfn "Possible hits: %A" (pHits |> List.map (fun s -> sprintf "%s at Row %i Col %i" s.Name s.Pos.Row s.Pos.Col))
-                    let found = pHits |> List.filter (fun segment -> segment.Pos = toBeHit.Pos)
-                    match found with
-                    | [] -> 
-                        power <- power + 1
-                    | _ ->
-                        let best = found |> List.minBy (fun segment -> segment.Ranking)
-                        if best.Ranking < bestRanking then
-                            bestRanking <- best.Ranking
-                            currentValue <- bestRanking * power
-                        power <- power + 1
-                if power > maxPower then
-                    counterTime <- counterTime + 1
-                    toBeHit <- { toBeHit with Pos = { Row = toBeHit.Pos.Row - 1; Col = toBeHit.Pos.Col - 1 } }   
-                    doContinue <- toBeHit.Pos.Col > 0 && toBeHit.Pos.Row >= 0
+            // Phase 2: Right
+            if denX2 <> 0.0 && denY2 <> 0.0 then
+                let tX = (fst to2 - fst from2) / denX2
+                let tY = (snd to2 - snd from2) / denY2
+                if areFloatsEqual tX tY && areFloatsEqual tX (round tX) && tX > 0.0 && tX <= p then
+                    result <- Some power
+            elif areFloatsEqual denY2 0.0 && areFloatsEqual (snd to2) (snd from2) && denX2 <> 0.0 then
+                let tX = (fst to2 - fst from2) / denX2
+                if areFloatsEqual tX (round tX) && tX > 0.0 && tX <= p then
+                    result <- Some power
 
-              
-                            
-            destroy tail (value + currentValue)            
+            if result.IsNone then
+                let from3 = (fst from2 + p * rX, snd from2 + p * rY)
+                let to3 = (fst to2 + m * p * dlX, snd to2 + m * p * dlY)
+                let (drX, drY) = vectDR
+                let denX3 = drX - m * dlX
+                let denY3 = drY - m * dlY
 
-    destroy targets 0
+                // Phase 3: Down-Right
+                if denX3 <> 0.0 && denY3 <> 0.0 then
+                    let tX = (fst to3 - fst from3) / denX3
+                    let tY = (snd to3 - snd from3) / denY3
+                    if areFloatsEqual tX tY && areFloatsEqual tX (round tX) && tX > 0.0 then
+                        result <- Some power
+                elif areFloatsEqual denY3 0.0 && areFloatsEqual (snd to3) (snd from3) && denX3 <> 0.0 then
+                    let tX = (fst to3 - fst from3) / denX3
+                    if tX > 0.0 && areFloatsEqual tX (round tX) && (snd to3 + tX <= 2.0) then
+                        result <- Some power
+        
+        power <- power + 1
+    
+    result
 
-//let destroyTargets (tower: Tower) (targets: Target list) =
-//    let floorline = -1
-//    let rec destroy (toDestroy: Target list) (value: int) =
-//        match toDestroy with
-//        | [] ->
-//            value
-//        | head :: tail ->
-//            let mutable power = 1
-//            let mutable destroyed = false
-//            let mutable newValue = value
-//            let mutable timer = 0
-//            let mutable toBeHit = head
-//            let mutable oldHits = []
-//            let mutable currentRanking = 4
-//            let mutable currentValue = 0
-//            while not destroyed do
-//                let pHits = possibleHits tower power timer floorline
-//                if oldHits = pHits then
-//                    timer <- timer + 1
-//                    power <- 1
-//                    toBeHit <- { toBeHit with Pos = { Row = toBeHit.Pos.Row - 1; Col = toBeHit.Pos.Col - 1 } }
-//                else
-//                    oldHits <- pHits
+let rank (catapult: Entity) (power: int) (target: Entity) =
+    let catapultValue = int catapult.Val - int 'A' + 1
+    let targetMultiplier = if target.Val = 'H' then 2 else 1
+    catapultValue * power * targetMultiplier
 
-//                    if pHits.Length = 0 then
-//                        power <- power + 1
-//                    else
-//                        if pHits |> List.forall (fun segment -> segment.Pos.Col > toBeHit.Pos.Col) then
-//                            timer <- timer + 1
-//                            power <- 1
-//                            toBeHit <- { toBeHit with Pos = { Row = toBeHit.Pos.Row - 1; Col = toBeHit.Pos.Col - 1 } }
-//                        else
-//                            let found = pHits |> List.tryFind (fun segment -> segment.Pos = toBeHit.Pos)
-//                            match found with
-//                            | None ->
-//                                power <- power + 1                
-//                            | Some segment ->
-//                                if currentRanking > segment.Ranking then
-//                                    currentRanking <- segment.Ranking
-//                                    currentValue <- currentRanking * power
-//                                power <- power + 1                                                                
-//                                if destroyed then
-//                                    newValue <- newValue + currentValue
-//                                    currentRanking <- 4
-//                                    currentValue <- 0
-//                destroyed <- toBeHit.Pos.Col <= 0 || toBeHit.Pos.Row <= 0
+let solve (catapults: Entity list) (targets: Entity list) (movingTargets: int) =
+    let folder (sum: int option) (target: Entity) =
+        match sum with
+        | None -> None // If sum is already "infinity", propagate it
+        | Some currentSum ->
+            let aPos = (catapults |> List.find (fun c -> c.Val = 'A')).Pos
+            let dist =
+                if movingTargets = 1 then abs(snd aPos - snd target.Pos)
+                else abs(fst aPos - fst target.Pos)
 
-//            destroy tail newValue
+            let maxPower = int (ceil (dist * 1.5))
 
-//    destroy targets 0
+            let ranks =
+                catapults
+                |> List.choose (fun catapult ->
+                    canHitTarget catapult.Pos target.Pos movingTargets 0 maxPower
+                    |> Option.map (fun power -> rank catapult power target)
+                )
 
-//let destroyTargets (tower: Tower) (targets: Target list) =
-//    let floorline = (tower.Segments |> List.head).Pos.Row + 1
-//    let rec destroy (toDestroy: Target list) (value: int) =
-//        match toDestroy with
-//        | [] ->
-//            value
-//        | head :: tail ->
-//            let mutable power = 1
-//            let mutable destroyed = false
-//            let mutable newValue = value
-//            let mutable life = head.Life
-//            while not destroyed do
-//                let hits = possibleHits tower power floorline
-//                let found = hits |> List.tryFind (fun (_, positions) -> positions |> List.contains head.Pos)
-//                match found with
-//                | None -> power <- power + 1
-//                | Some (segment, _) -> 
-//                    newValue <- newValue + (segment.Ranking * power); 
-//                    life <- life - 1
-//                    destroyed <- life = 0
-
-//            destroy tail newValue
-
-//    destroy targets 0
+            let finalRanks =
+                if not (List.isEmpty ranks) then ranks
+                else
+                    catapults
+                    |> List.choose (fun catapult ->
+                        canHitTarget catapult.Pos target.Pos movingTargets 1 maxPower
+                        |> Option.map (fun power -> rank catapult power target)
+                    )
+            
+            if not (List.isEmpty finalRanks) then
+                Some(currentSum + List.min finalRanks)
+            else
+                None // No hit possible, result becomes "infinity"
+    
+    let result = targets |> List.fold folder (Some 0)
+    result |> Option.defaultValue -1 // Using -1 to indicate failure/infinity
 
 let execute() =
-    let lines = LocalHelper.GetLinesFromFile(path)
-    let meteors = parseContent lines
-    let tower = {
-        Segments = [
-            {
-                Name = "C";
-                Ranking = 3;
-                Pos = {Row = 2; Col = 0}
-            };
-            {
-                Name = "B";
-                Ranking = 2;
-                Pos = {Row = 1; Col = 0}
-            };
-            {
-                Name = "A";
-                Ranking = 1;
-                Pos = {Row = 0; Col = 0}
-            }
-        ]
-     }
-    destroyTargets tower meteors
+    let catapultLayout = """
+.C...........
+.B...........
+.A...........
+==============
+"""
+    let targetLines = LocalHelper.GetLinesFromFile(path)
+    let catapults, targets = parseContent catapultLayout targetLines
+    
+    solve catapults targets 1
--- README.md
+++ README.md
@@ -8,7 +8,7 @@ https://everybody.codes/
 🥚 = 0 parts
 | Year | Title | Progress | Score |
 | - | - | - | - |
-| 2024 | The Kingdom of Algorithmia | 🐔🐔🐔🐔🐔🐔🐔🐔🐔🐔🐔🐥🐔🐔🐔🐔🐥🐔🐔🥚 | (55/60) |
+| 2024 | The Kingdom of Algorithmia | 🐔🐔🐔🐔🐔🐔🐔🐔🐔🐔🐔🐔🐔🐔🐔🐔🐥🐔🐔🥚 | (56/60) |
 | 2024 S1 | Echoes of Enigmatus No. 1 | 🥚🥚🥚 | (0/12) |
 | 2024 S2 | The Entertainment Hub No. 2 | 🥚🥚🥚 | (0/12) |
 | 2025 | The Song of Ducks and Dragons | 🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚🥚 | (0/60) | 
@@ -17,7 +17,7 @@ https://everybody.codes/
 
 [https://everybody.codes/event/2024](https://everybody.codes/event/2024/quests)
 
-(18.33/20)
+(18.66/20)
 
 |Day      | Part 1 | Part 2 | Part 3 |
 |---------|----|----|----|
@@ -32,7 +32,7 @@ https://everybody.codes/
 | Quest 9   | 🐤 [F#](./EverybodyCodes_2024_FSharp/quest09/quest09_1.fs) | 🐤 [F#](./EverybodyCodes_2024_FSharp/quest09/quest09_2.fs) | 🐤 [F#](./EverybodyCodes_2024_FSharp/quest09/quest09_3.fs) |
 | Quest 10  | 🐤 [F#](./EverybodyCodes_2024_FSharp/quest10/quest10_1.fs) | 🐤 [F#](./EverybodyCodes_2024_FSharp/quest10/quest10_2.fs) | 🐤 [F#](./EverybodyCodes_2024_FSharp/quest10/quest10_3.fs) |
 | Quest 11  | 🐤 [F#](./EverybodyCodes_2024_FSharp/quest11/quest11_1.fs) | 🐤 [F#](./EverybodyCodes_2024_FSharp/quest11/quest11_2.fs) | 🐤 [F#](./EverybodyCodes_2024_FSharp/quest11/quest11_3.fs) |
-| Quest 12  | 🐤 [F#](./EverybodyCodes_2024_FSharp/quest12/quest12_1.fs) | 🐤 [F#](./EverybodyCodes_2024_FSharp/quest12/quest12_2.fs) | ❌ |
+| Quest 12  | 🐤 [F#](./EverybodyCodes_2024_FSharp/quest12/quest12_1.fs) | 🐤 [F#](./EverybodyCodes_2024_FSharp/quest12/quest12_2.fs) | 🐤 [F#](./EverybodyCodes_2024_FSharp/quest12/quest12_3.fs) |
 | Quest 13  | 🐤 [F#](./EverybodyCodes_2024_FSharp/quest13/quest13_1.fs) | 🐤 [F#](./EverybodyCodes_2024_FSharp/quest13/quest13_2.fs) | 🐤 [F#](./EverybodyCodes_2024_FSharp/quest13/quest13_3.fs) |
 | Quest 14  | 🐤 [F#](./EverybodyCodes_2024_FSharp/quest14/quest14_1.fs) | 🐤 [F#](./EverybodyCodes_2024_FSharp/quest14/quest14_2.fs) | 🐤 [F#](./EverybodyCodes_2024_FSharp/quest14/quest14_3.fs) |
 | Quest 15  | 🐤 [F#](./EverybodyCodes_2024_FSharp/quest15/quest15_1.fs) | 🐤 [F#](./EverybodyCodes_2024_FSharp/quest15/quest15_2.fs) | 🐤 [F#](./EverybodyCodes_2024_FSharp/quest15/quest15_3.fs) |
