652365aa081609337a340db166be68e98524fede
blfuentes <epiphonetodie@gmail.com>
2025-11-07 09:19:55 +0100

    working: 20250403

--- EverybodyCodes_2025_FSharp/quest04/quest04_1.fs
+++ EverybodyCodes_2025_FSharp/quest04/quest04_1.fs
@@ -2,9 +2,29 @@ module quest04_part01
 
 open EverybodyCodes_2025_FSharp.Modules
 
-let path = "quest04/test_input_01.txt"
-//let path = "quest04/quest04_input_01.txt"
+//let path = "quest04/test_input_01.txt"
+let path = "quest04/quest04_input_01.txt"
+
+let parseContent(lines: string array) =
+    lines |> Array.map float
+
+let calculateRatio ((g1, g2): float*float) =
+    g1 / g2
+
+let calculateCycle (gears: float array) =
+    let oneTurn =
+        gears
+        |> Array.pairwise
+        |> Array.map calculateRatio
+        |> Array.reduce (*)
+    oneTurn
+
+let calcuateTarget (gears: float array) (target: float) =
+    let oneTurn = calculateCycle gears
+    target * oneTurn
+
 
 let execute() =
     let lines = LocalHelper.GetLinesFromFile(path)
-    0
+    let gears = parseContent lines
+    sprintf "%.0f" (System.Math.Floor(calcuateTarget gears 2025))
--- EverybodyCodes_2025_FSharp/quest04/quest04_2.fs
+++ EverybodyCodes_2025_FSharp/quest04/quest04_2.fs
@@ -2,9 +2,31 @@ module quest04_part02
 
 open EverybodyCodes_2025_FSharp.Modules
 
-let path = "quest04/test_input_02.txt"
-//let path = "quest04/quest04_input_02.txt"
+//let path = "quest04/test_input_02.txt"
+let path = "quest04/quest04_input_02.txt"
+
+let parseContent(lines: string array) =
+    lines |> Array.map decimal
+
+let calculateRatio ((g1, g2): decimal*decimal) =
+    g1 / g2
+
+let calculateCycle (gears: decimal array) =
+    let oneTurn =
+        gears
+        |> Array.pairwise
+        |> Array.map calculateRatio
+        |> Array.reduce (*)
+    oneTurn
+
+let calcuateTarget (gears: decimal array) (target: decimal) =
+    let oneTurn = calculateCycle (gears |> Array.rev)
+    target * oneTurn
+
 
 let execute() =
     let lines = LocalHelper.GetLinesFromFile(path)
-    0
+    let gears = parseContent lines
+    let value = System.Math.Ceiling(calcuateTarget gears (decimal 10000000000000.))
+    let formatted = value.ToString("F0", System.Globalization.CultureInfo.InvariantCulture)
+    sprintf "%s" formatted
