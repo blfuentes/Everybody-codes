53296753c2ee228e9303c305e6868a02f911905f
blfuentes <epiphonetodie@gmail.com>
2025-11-14 08:59:42 +0100

    working: 2025093

--- EverybodyCodes_2025_FSharp/quest09/quest09_1.fs
+++ EverybodyCodes_2025_FSharp/quest09/quest09_1.fs
@@ -2,9 +2,59 @@ module quest09_part01
 
 open EverybodyCodes_2025_FSharp.Modules
 
-let path = "quest09/test_input_01.txt"
-//let path = "quest09/quest09_input_01.txt"
+//let path = "quest09/test_input_01.txt"
+let path = "quest09/quest09_input_01.txt"
+
+let parseContent (lines: string array) =
+    let scales =
+        lines
+        |> Array.map(fun line ->
+            let id = int(line.Split(":")[0])
+            let dna = (line.Split(":")[1]).ToCharArray()
+            (id, dna)
+        )
+    scales |> Map.ofArray
+
+let isChild (childIdx: int) (scales: Map<int, char array>) =
+    let child = scales[childIdx]
+    let parentKeys = scales.Keys |> Seq.filter(fun k -> k <> childIdx)
+    let rec matches(index: int) =
+        if index = child.Length then
+            true
+        else
+            let childChar = child[index]
+            let parentChars =
+                parentKeys
+                |> Seq.map(fun k -> scales[k][index])
+                |> Seq.toArray
+            match Array.contains childChar parentChars with
+            | true -> matches(index + 1)
+            | false -> false
+    matches 0
+
+let buildFamily(scales: Map<int, char array>) =
+    let child = 
+        scales
+        |> Map.keys
+        |> Seq.find(fun k -> isChild k scales)
+    ((child, scales[child]), scales |> Map.remove child)
+
+let calculateSimilarity(childDna: char array) (parentDnas: Map<int, char array>) =
+    let rec similarityHelper(index: int, parents: int array) =
+        if index = childDna.Length then
+            parents |> Array.fold(fun acc p -> acc * p) 1
+        else
+            parentDnas
+            |> Map.toSeq
+            |> Seq.iteri(fun i (_, parentDna) ->
+                if childDna[index] = parentDna[index] then
+                    parents[i] <- parents[i] + 1
+            )
+            similarityHelper(index + 1, parents)
+    similarityHelper(0, [|0; 0|])
 
 let execute() =
     let lines = LocalHelper.GetLinesFromFile(path)
-    0
+    let scales = parseContent lines
+    let (child, parents) = buildFamily scales
+    calculateSimilarity (snd child) parents
--- EverybodyCodes_2025_FSharp/quest09/quest09_3.fs
+++ EverybodyCodes_2025_FSharp/quest09/quest09_3.fs
@@ -5,6 +5,88 @@ open EverybodyCodes_2025_FSharp.Modules
 let path = "quest09/test_input_03.txt"
 //let path = "quest09/quest09_input_03.txt"
 
+let parseContent (lines: string array) =
+    let scales =
+        lines
+        |> Array.map(fun line ->
+            let id = int(line.Split(":")[0])
+            let dna = (line.Split(":")[1]).ToCharArray()
+            (id, dna)
+        )
+    scales |> Map.ofArray
+
+let isChild (childIdx: int) (scales: Map<int, char array>) =
+    let child = scales[childIdx]
+    let parentKeys = scales.Keys |> Seq.filter(fun k -> k <> childIdx)
+    let rec matches(index: int) =
+        if index = child.Length then
+            true
+        else
+            let childChar = child[index]
+            let parentChars =
+                parentKeys
+                |> Seq.map(fun k -> scales[k][index])
+                |> Seq.toArray
+            match Array.contains childChar parentChars with
+            | true -> matches(index + 1)
+            | false -> false
+    matches 0
+
+let buildFamily(scales: Map<int, char array>) =
+    let child = 
+        scales
+        |> Map.keys
+        |> Seq.tryFind(fun k -> isChild k scales)
+    match child with
+    | None -> (None, None)
+    | Some child ->
+        (Some (child, scales[child]), Some(scales |> Map.remove child))
+
+let calculateSimilarity(childDna: char array) (parentDnas: Map<int, char array>) =
+    let rec similarityHelper(index: int, parents: int array) =
+        if index = childDna.Length then
+            parents |> Array.fold(fun acc p -> acc * p) 1
+        else
+            parentDnas
+            |> Map.toSeq
+            |> Seq.iteri(fun i (_, parentDna) ->
+                if childDna[index] = parentDna[index] then
+                    parents[i] <- parents[i] + 1
+            )
+            similarityHelper(index + 1, parents)
+    similarityHelper(0, [|0; 0|])
+
+let rec comb n l =
+    match (n,l) with
+    | (0,_) -> [[]]
+    | (_,[]) -> []
+    | (n,x::xs) ->
+        let useX = List.map (fun l -> x::l) (comb (n-1) xs)
+        let noX = comb n xs
+        (useX @ noX)
+
+let buildFamilies(scales: Map<int, char array>) =
+    let keys = scales |> Map.keys |> Seq.toArray
+    let combinations = comb 3 (Array.toList keys)
+    let families =
+        combinations
+        |> List.map(fun comb ->
+            let possibleFamily = scales |> Map.filter(fun k _ -> List.contains k comb)
+            let family = buildFamily possibleFamily
+            match family with
+            | (Some c, Some p) -> 
+                (true, c, p)
+            | _ -> 
+                (false, (0,[||]), Map.empty)
+        )
+    families
+    |> List.filter(fun (isFamily, _, _) -> isFamily)
+    |> List.sumBy(fun f ->
+        let (_, (_, child), parents) = f
+        calculateSimilarity child parents
+    )
+
 let execute() =
     let lines = LocalHelper.GetLinesFromFile(path)
-    0
+    let scales = parseContent lines
+    buildFamilies scales
