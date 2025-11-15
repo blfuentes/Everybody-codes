a898160523a82fb5c3729765eef7a1e969627485
blfuentes <epiphonetodie@gmail.com>
2025-11-15 10:34:22 +0100

    finished/working: 2025102/2025103

--- EverybodyCodes_2025_FSharp/quest10/quest10_1.fs
+++ EverybodyCodes_2025_FSharp/quest10/quest10_1.fs
@@ -2,9 +2,107 @@ module quest10_1
 
 open EverybodyCodes_2025_FSharp.Modules
 
-let path = "quest10/test_input_01.txt"
-//let path = "quest10/quest10_input_01.txt"
+//let path = "quest10/test_input_01.txt"
+let path = "quest10/quest10_input_01.txt"
+
+type CellType =
+    | Empty
+    | Sheep
+    | Dragon
+
+type Position = {
+    X: int;
+    Y: int
+}
+
+type Character = {
+    Pos: Position;
+    Kind: CellType
+}
+
+type FieldMap = {
+    Width: int;
+    Height: int;
+    Characters: Character list
+}
+
+let parseContent(lines: string array) =
+    let maxX = lines[0].Length
+    let maxY = lines.Length
+    let characters =
+        lines
+        |> Array.mapi (fun y line ->
+            line.ToCharArray()
+            |> Array.mapi (fun x ch ->
+                match ch with
+                | '.' -> None
+                | 'S' -> Some { Pos = { X = x; Y = y } ; Kind = Sheep }
+                | 'D' -> Some { Pos = { X = x; Y = y } ; Kind = Dragon }
+                | _ -> None
+            )
+            |> Array.choose id
+        )
+        |> Array.concat
+        |> Array.toList
+    {
+        Width = maxX;
+        Height = maxY;
+        Characters = characters
+    }
+
+let printFieldMap(fieldMap: FieldMap) =
+    for y in 0 .. fieldMap.Height - 1 do
+        let line =
+            [ for x in 0 .. fieldMap.Width - 1 do
+                let ch =
+                    match fieldMap.Characters |> List.tryFind (fun c -> c.Pos.X = x && c.Pos.Y = y) with
+                    | Some character ->
+                        match character.Kind with
+                        | Sheep -> 'S'
+                        | Dragon -> 'D'
+                    | None -> '.'
+                yield ch
+            ] |> System.String.Concat
+        printfn "%s" line
+
+let findNewPositions(pos: Position, fieldMap: FieldMap): Position list =
+    let deltas = [(-2, -1); (-2, 1); (2, -1); (2, 1); (-1, -2); (-1, 2); (1, -2); (1, 2)];
+    let positions =
+        deltas
+        |> List.map (fun (dx, dy) -> { X = pos.X + dx; Y = pos.Y + dy })
+        |> List.filter (fun pos ->
+            pos.X >= 0 && pos.X < fieldMap.Width &&
+            pos.Y >= 0 && pos.Y < fieldMap.Height
+        )
+    positions
+
+let move(fieldMap: FieldMap) (numOfMoves: int): Position list =
+    let rec moveCharacter (positions: Position list, movesLeft: int) (visited: Position list): Position list =
+        if movesLeft = 0 then
+            visited
+        else
+            let newPositions = 
+                positions
+                |> List.collect (fun pos -> findNewPositions(pos, fieldMap))
+                |> List.filter (fun pos -> not (List.contains pos visited))
+            moveCharacter(newPositions, movesLeft - 1) (visited @ newPositions)
+    let dragon = fieldMap.Characters |> List.find (fun c -> c.Kind = Dragon)
+    let positions = moveCharacter([dragon.Pos], numOfMoves) []
+    positions
+
+let haunt(fieldMap: FieldMap) (dragonPositions: Position list) : int =
+    let sheepPositions =
+        fieldMap.Characters
+        |> List.filter (fun c -> c.Kind = Sheep)
+        |> List.map (fun c -> c.Pos)
+        |> Set.ofList
+    let setDragonPositions = Set.ofList dragonPositions
+    (Set.intersect sheepPositions setDragonPositions).Count
 
 let execute() =
     let lines = LocalHelper.GetLinesFromFile(path)
-    0
+    let fieldMap = parseContent(lines)
+    let dragonPositions = move fieldMap 4
+    haunt fieldMap dragonPositions
+    //printFieldMap fieldMap
+    //fieldMap.Characters.Length
--- EverybodyCodes_2025_FSharp/quest10/quest10_2.fs
+++ EverybodyCodes_2025_FSharp/quest10/quest10_2.fs
@@ -2,9 +2,144 @@ module quest10_2
 
 open EverybodyCodes_2025_FSharp.Modules
 
-let path = "quest10/test_input_02.txt"
-//let path = "quest10/quest10_input_02.txt"
+//let path = "quest10/test_input_02.txt"
+let path = "quest10/quest10_input_02.txt"
+
+type CellType =
+    | Empty
+    | Sheep
+    | Dragon
+    | Hideout
+
+type Position = {
+    X: int;
+    Y: int
+}
+
+type Character = {
+    Pos: Position;
+    Kind: CellType
+}
+
+type FieldMap = {
+    Width: int;
+    Height: int;
+    Dragons: Set<Position>;
+    Sheeps: Set<Position>;
+    Hideouts: Set<Position>;
+}
+
+let parseContent (lines: string array) =
+    let maxX = lines[0].Length
+    let maxY = lines.Length
+    let characters =
+        lines
+        |> Array.mapi (fun y line ->
+            line.ToCharArray()
+            |> Array.mapi (fun x ch ->
+                match ch with
+                | '.' -> None
+                | 'S' -> Some { Pos = { X = x; Y = y } ; Kind = Sheep }
+                | 'D' -> Some { Pos = { X = x; Y = y } ; Kind = Dragon }
+                | '#' -> Some { Pos = { X = x; Y = y } ; Kind = Hideout }
+                | _ -> None
+            )
+            |> Array.choose id
+        )
+        |> Array.concat
+        |> Array.toList
+    {
+        Width = maxX;
+        Height = maxY;
+        Dragons = characters |> List.filter (fun c -> c.Kind = Dragon) |> List.map (fun c -> c.Pos) |> Set.ofList;
+        Sheeps = characters |> List.filter (fun c -> c.Kind = Sheep) |> List.map (fun c -> c.Pos) |> Set.ofList;
+        Hideouts = characters |> List.filter (fun c -> c.Kind = Hideout) |> List.map (fun c -> c.Pos) |> Set.ofList;
+    }
+
+let printFieldMap (fieldMap: FieldMap) =
+    for y in 0 .. fieldMap.Height - 1 do
+        let line =
+            [ for x in 0 .. fieldMap.Width - 1 do
+                let ch =
+                    if fieldMap.Dragons |> Set.contains { X = x; Y = y } then
+                        'D'
+                    elif fieldMap.Sheeps |> Set.contains { X = x; Y = y } then
+                        'S'
+                    elif fieldMap.Hideouts |> Set.contains { X = x; Y = y } then
+                        'H'
+                    else
+                        '#'
+                yield ch
+            ] |> System.String.Concat
+        printfn "%s" line
+
+let findNewPositions (character: Character) : Position list =
+    let deltas = 
+        if character.Kind.IsDragon then
+            [(-2, -1); (-2, 1); (2, -1); (2, 1); (-1, -2); (-1, 2); (1, -2); (1, 2)]
+        else
+            [(0, 1)]            
+    
+    deltas
+    |> List.map (fun (dx, dy) -> { X = character.Pos.X + dx; Y = character.Pos.Y + dy })
+    
+
+let haunt (fieldMap: FieldMap): Set<Position> =
+    Set.difference (Set.intersect fieldMap.Dragons fieldMap.Sheeps) fieldMap.Hideouts 
+
+let moveAndHaunt (fieldMap: FieldMap) (numOfMoves: int): int =
+    let rec moveCharacter (currentMap: FieldMap) (movesLeft: int) (haunted: int) : int =
+        if movesLeft = 0 then
+            haunted
+        else
+            // find new positions of the dragon
+            let newDragonPositions = 
+                currentMap.Dragons
+                |> Seq.collect (fun pos -> 
+                    findNewPositions({ Pos = pos; Kind = Dragon })
+                    |> List.filter (fun newPos -> 
+                        newPos.X >= 0 && newPos.X < currentMap.Width &&
+                        newPos.Y >= 0 && newPos.Y < currentMap.Height
+                    )
+                ) |> Set.ofSeq
+            
+            let dragonMap = { currentMap with Dragons = newDragonPositions }
+            
+            // find haunted sheeps on dragon movement
+            let hauntedByDragon = haunt dragonMap
+            
+            let sheepsAfterDragonHaunt =
+                Set.difference currentMap.Sheeps hauntedByDragon
+            
+            let sheepMap = { dragonMap with Sheeps = sheepsAfterDragonHaunt }
+
+            // find new positions of the sheeps
+            let newSheepPositions =
+                sheepMap.Sheeps
+                |> Seq.collect (fun pos ->
+                    findNewPositions({ Pos = pos; Kind = Sheep })
+                    |> List.filter (fun newPos ->
+                        newPos.X >= 0 && newPos.X < sheepMap.Width &&
+                        newPos.Y >= 0 && newPos.Y < sheepMap.Height
+                    )
+                ) |> Set.ofSeq
+            let sheepMapAfterMove = { sheepMap with Sheeps = newSheepPositions }
+
+            // find haunted sheeps on sheep movement
+            let hauntedBySheep = haunt sheepMapAfterMove
+            let totalHaunted = haunted + hauntedByDragon.Count + hauntedBySheep.Count
+
+            let sheepsAfterSheepHaunt =
+                Set.difference sheepMapAfterMove.Sheeps hauntedBySheep
+
+            let newMap = { sheepMapAfterMove with Sheeps = sheepsAfterSheepHaunt }
+            moveCharacter newMap (movesLeft - 1) totalHaunted            
+
+    let positions = moveCharacter fieldMap numOfMoves 0
+    positions
+
 
 let execute() =
     let lines = LocalHelper.GetLinesFromFile(path)
-    0
+    let fieldMap = parseContent(lines)
+    moveAndHaunt fieldMap 20
