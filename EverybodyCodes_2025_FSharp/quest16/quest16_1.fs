02a277d4d69372f709c1ac10cdc511cdcd7bd74b
blfuentes <epiphonetodie@gmail.com>
2025-11-25 07:41:48 +0100

    working: 202516

--- EverybodyCodes_2025_FSharp/quest16/quest16_1.fs
+++ EverybodyCodes_2025_FSharp/quest16/quest16_1.fs
@@ -2,9 +2,19 @@ module quest16_1
 
 open EverybodyCodes_2025_FSharp.Modules
 
-let path = "quest16/test_input_01.txt"
-//let path = "quest16/quest16_input_01.txt"
+//let path = "quest16/test_input_01.txt"
+let path = "quest16/quest16_input_01.txt"
+
+let parseContent(lines: string) =
+    lines.Split(",") |> Array.map int
+
+let calculateWall (instructions: int[]) (wallLength: int) =
+    instructions
+    |> Array.sumBy(fun i -> 
+        wallLength / i
+    )
 
 let execute() =
-    let lines = LocalHelper.GetLinesFromFile(path)
-    0
+    let lines = LocalHelper.GetContentFromFile(path)
+    let instructions = parseContent(lines)
+    calculateWall instructions 90
--- EverybodyCodes_2025_FSharp/quest16/quest16_2.fs
+++ EverybodyCodes_2025_FSharp/quest16/quest16_2.fs
@@ -2,9 +2,24 @@ module quest16_2
 
 open EverybodyCodes_2025_FSharp.Modules
 
-let path = "quest16/test_input_02.txt"
-//let path = "quest16/quest16_input_02.txt"
+//let path = "quest16/test_input_02.txt"
+let path = "quest16/quest16_input_02.txt"
+
+let parseContent(lines: string) =
+    lines.Split(",") |> Array.map(fun v -> bigint.Parse(v))
+
+let deconstructWall (numbers: bigint[]) =
+    []
+
+
+let calculateWall (instructions: bigint[]) =
+    let wall = deconstructWall instructions
+    wall
+    |> List.reduce(fun acc i-> 
+        acc * i
+    )
 
 let execute() =
-    let lines = LocalHelper.GetLinesFromFile(path)
-    0
+    let lines = LocalHelper.GetContentFromFile(path)
+    let instructions = parseContent(lines)
+    calculateWall instructions
