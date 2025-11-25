a37bc53953f04336d507cf680654a448e49bc18c
blfuentes <epiphonetodie@gmail.com>
2025-11-25 09:45:21 +0100

    finished: 2025162

--- EverybodyCodes_2025_FSharp/quest16/quest16_2.fs
+++ EverybodyCodes_2025_FSharp/quest16/quest16_2.fs
@@ -8,14 +8,27 @@ let path = "quest16/quest16_input_02.txt"
 let parseContent(lines: string) =
     lines.Split(",") |> Array.map(fun v -> bigint.Parse(v))
 
-let deconstructWall (numbers: bigint[]) =
-    []
+let deconstructWall (towers: bigint[]) =
+    let spells = ResizeArray<bigint>()
+    let simulated = Array.zeroCreate<bigint> (towers.Length)
+    let wallLength = towers.Length
 
+    towers
+    |> Array.iteri(fun index towerHeight ->
+        let tempHeight = simulated[index]
+        if tempHeight < towerHeight then
+            let diff = int(towerHeight - tempHeight)
+            let pos = index + 1
+            spells.Add(pos)
+            for j in index..pos..wallLength-1 do
+                simulated[j] <- simulated[j] + bigint(diff)
+    )   
+    spells
 
 let calculateWall (instructions: bigint[]) =
-    let wall = deconstructWall instructions
-    wall
-    |> List.reduce(fun acc i-> 
+    let spells = deconstructWall instructions
+    spells
+    |> Seq.reduce(fun acc i-> 
         acc * i
     )
 
--- EverybodyCodes_2025_FSharp/quest16/quest16_3.fs
+++ EverybodyCodes_2025_FSharp/quest16/quest16_3.fs
@@ -7,4 +7,5 @@ let path = "quest16/test_input_03.txt"
 
 let execute() =
     let lines = LocalHelper.GetLinesFromFile(path)
+    let blocksToUse = 202520252025000I
     0
