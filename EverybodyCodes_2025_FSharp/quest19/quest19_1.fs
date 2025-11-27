f30e5495787a4cc9b15b28263b73588910199326
blfuentes <epiphonetodie@gmail.com>
2025-11-27 10:32:53 +0100

    prepared: 202519

---
+++ EverybodyCodes_2025_FSharp/quest19/quest19_1.fs
@@ -0,0 +1,10 @@
+module quest19_1
+
+open EverybodyCodes_2025_FSharp.Modules
+
+let path = "quest19/test_input_01.txt"
+//let path = "quest19/quest19_input_01.txt"
+
+let execute() =
+    let lines = LocalHelper.GetLinesFromFile(path)
+    0
---
+++ EverybodyCodes_2025_FSharp/quest19/quest19_3.fs
@@ -0,0 +1,10 @@
+module quest19_3
+
+open EverybodyCodes_2025_FSharp.Modules
+
+let path = "quest19/test_input_03.txt"
+//let path = "quest19/quest19_input_03.txt"
+
+let execute() =
+    let lines = LocalHelper.GetLinesFromFile(path)
+    0
